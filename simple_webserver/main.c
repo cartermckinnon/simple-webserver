2994fa9d04df6630f156bfcd88b4d53abf4a9d5e
Carter McKinnon <cartermckinnon@gmail.com>
2016-11-04 01:40:37 +0900

    Initial Commit

---
+++ simple_webserver/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  simple_webserver
+//
+//  Created by Carter McKinnon on 11/4/16.
+//  Copyright © 2016 Carter McKinnon. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
